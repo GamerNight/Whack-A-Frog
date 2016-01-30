3e18369307606e758dd0774ad0e3cef35f1b9894
computeristgeek <salahuddin.kazak@gmail.com>
2016-01-30 16:37:14 +0200

    Randomize Frog Jumps

--- AnimatedSprite.gd
+++ AnimatedSprite.gd
@@ -2,18 +2,23 @@
 extends AnimatedSprite
 
 var initpos=get_pos()
+var startTime=0
 
 func _ready():
 	set_process(true)
-	print(str(initpos))
-
 	pass
 
 func _process(delta):
+	print(initpos)
 	var anim=get_node("AnimationPlayer").get_animation("frogAnim")
 	anim.track_set_key_value(1,0,initpos)
 	anim.track_set_key_value(1,1,Vector2(initpos.x,initpos.y-100))
 	anim.track_set_key_value(1,2,initpos)
-	if(Input.is_action_pressed("ui_up")):
-		get_node("AnimationPlayer").play("frogAnim")
+	if(startTime>0.5):
+		startTime=0
+		if(int(self.get_name()) == (randi() % 17)):
+			self.get_node("AnimationPlayer").play("frogAnim")
+	else:
+		startTime+=delta
+
 
